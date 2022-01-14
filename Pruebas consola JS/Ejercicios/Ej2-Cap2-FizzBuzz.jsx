7f12299db794c24e7e6f468d0d4e435f10ed2c03
bidaux <bidaux3@hotmail.es>
2022-01-14 01:35:34 +0100

    Update

--- Pruebas consola JS/Ejercicios/Ej2-Cap2-FizzBuzz.jsx
+++ Pruebas consola JS/Ejercicios/Ej2-Cap2-FizzBuzz.jsx
@@ -4,3 +4,17 @@ for (let num = 1; num <= 100; num++) {
     if (num % 5 == 0) output += "Buzz";
   console.log(output);
   }
+
+  let tam = 8;
+  let tab = " ";
+  for (let bl = 0; bl < tam; bl++){
+    for(let ng = 0; ng < tam; ng++){
+      if (( bl + ng) % 2 == 0) {
+        tab += " ";
+      } else {
+        tab += "#";
+      }
+    }
+  tab += "\n";
+  }
+  console.log(tab);
